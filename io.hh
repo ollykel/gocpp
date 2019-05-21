bd069b2ecd2a86b54ce51a51b9963ae90219ab22
OKelt <oakelton@gmail.com>
2019-05-21 03:54:51 -0700

    created abstract classes Writer, Reader, ReadWriter

--- io.hh
+++ io.hh
@@ -8,11 +8,23 @@
  * Reader, Writer, ReadWriter, etc. as abstract classes.
  */
 
-
+#include <iostream>
 
 using namespace std;
 
+namespace io {
+	class Writer {
+		public:
+		virtual size_t write (void *src, size_t len) = 0;//-- end size_t write
+	};//-- enc class Writer
 
+	class Reader {
+		public:
+		virtual size_t read (void *dest, size_t len) = 0;//-- end size_t read
+	};//-- end class Reader
+
+	class ReadWriter: public Reader, public Writer {};//-- end class ReadWriter
+};//-- end namespace io
 
 #endif
 
