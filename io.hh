7f391de3a59ed39b42196a22364d3f41ddf9b019
OKelt <oakelton@gmail.com>
2019-05-21 09:59:23 -0700

    added files to io.hh

--- io.hh
+++ io.hh
@@ -9,6 +9,7 @@
  */
 
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -18,12 +19,61 @@ namespace io {
 		virtual size_t write (void *src, size_t len) = 0;//-- end size_t write
 	};//-- enc class Writer
 
+	class WriterTo {
+		public:
+		virtual size_t write_to (Writer& w) = 0;
+	};//-- end class WriterTo
+
 	class Reader {
 		public:
 		virtual size_t read (void *dest, size_t len) = 0;//-- end size_t read
 	};//-- end class Reader
 
+	class ReaderFrom {
+		public:
+		virtual size_t read_from (Reader& r) = 0;
+	};//-- end class ReaderFrom
+
+	class Seeker {
+		public:
+		virtual size_t seek (size_t offset, int whence) = 0;
+	};//-- end class Seeker
+
 	class ReadWriter: public Reader, public Writer {};//-- end class ReadWriter
+
+	size_t copy_n (Writer& dst, Reader& src, size_t n) {
+		char *buffer = new char [n];
+		size_t len = src.read(buffer, n);
+		len = dst.write(buffer, len);
+		delete[] buffer;
+		return len;
+	}//-- end size_t copy_n
+
+	class File {
+		protected:
+		FILE *src;
+
+		public:
+		~File () {
+			fclose(this->src);
+		}//-- end destructor
+	};//-- end class File
+
+	class RFile: public File, public Reader {
+		public:
+		size_t read (void *dest, size_t len) {
+			size_t n = fread(dest, len, 1, this->src);
+			return n;
+		}//-- end size_t read
+	};//-- end class RFIile
+
+	class WFile: public File, public Writer {
+		public:
+		size_t write (void *src, size_t len) {
+			size_t n = fwrite(src, len, 1, this->src);
+			return n;
+		}//-- end size_t write
+	};//-- end class WFile
 };//-- end namespace io
 
 #endif
