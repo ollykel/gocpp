ec86767d42792b8bb927bd8d72a08b98b70f668f
OKelt <oakelton@gmail.com>
2019-05-21 03:12:51 -0700

    initialized io.hh

---
+++ README.md
@@ -0,0 +1,6 @@
+# Go C++
+Library of functions allowing for creation of Golang-like code in C++.
+
+Makes heavy use of multiple-inheritance of abstract classes to empulate
+Golang interfaces.
+
---
+++ io.hh
@@ -0,0 +1,18 @@
+#ifndef IO_HH
+#define IO_HH
+
+/**
+ * @author Oliver Kelton, oakelton@gmail_com
+ * @date May 21, 2019
+ * C++ approximation of Golang's io package, including interfaces such as
+ * Reader, Writer, ReadWriter, etc. as abstract classes.
+ */
+
+
+
+using namespace std;
+
+
+
+#endif
+
