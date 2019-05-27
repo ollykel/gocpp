9e7c016e8ca6fcbb959507802ff3e84ada881b0e
OKelt <oakelton@gmail.com>
2019-05-27 16:41:36 -0700

    removed allocator from containers.hh

--- containers.hh
+++ containers.hh
@@ -64,56 +64,6 @@ namespace cont {
 		}//-- end T pop
 	};//-- end class Stack
 
-	// Allocator
-
-	class AllocEmptyErr: public exception {
-		virtual const char *what () const throw () {
-			return "allocator out of memory";
-		}//-- end what
-	} alloc_empty;//-- end class AllocEmptyErr
-
-	template <class T, const size_t size>
-	class Allocator: public allocator<T> {
-		protected:
-		const T memory[size];
-		const T *limit;
-		T *curr;
-		Stack <T*, size> pool;
-		public:
-		Allocator () : memory(), limit(memory + size) {
-			this->curr = (T*) memory;
-		}//-- end constructor 1
-
-		T* allocate () {
-			if (!this->pool.is_empty()) {
-				return this->pool.pop();
-			}
-			if (this->curr == this->limit) throw alloc_empty;
-			T *out = this->curr;
-			this->curr++;
-			return out;
-		}//-- end T* alloc
-
-		T* allocate (size_t len) {
-			if (this->curr + len > this->limit) throw alloc_empty;
-			T *out = this->curr;
-			this->curr += len;
-			return out;
-		}//-- end T* alloc
-
-		void deallocate (T *item) {
-			this->pool.push(item);
-		}//-- end void deallocate
-
-		size_t capacity () {
-			return this->limit - this->curr + this->pool.size();
-		}//-- end size_t capacity
-
-		bool is_empty () {
-			return this->curr == this->limit && this->pool.is_empty();
-		}//-- end bool is_full
-	};//-- end class Allocator
-
 };//-- end namespace cont
 
 #endif
